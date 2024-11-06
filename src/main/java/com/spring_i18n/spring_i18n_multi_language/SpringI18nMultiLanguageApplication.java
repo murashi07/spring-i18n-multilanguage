ca6b485c4e16239922f38a4b5a438a54637305b4
murashi07 <yvanmurashi@gmail.com>
2024-11-06 13:23:42 +0200

    first commit

---
+++ src/main/java/com/spring_i18n/spring_i18n_multi_language/SpringI18nMultiLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.spring_i18n.spring_i18n_multi_language;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringI18nMultiLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringI18nMultiLanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_i18n/spring_i18n_multi_language/SpringI18nMultiLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_i18n.spring_i18n_multi_language;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringI18nMultiLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
